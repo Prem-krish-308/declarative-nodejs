e0dbf61de09d12dbca0bc6a427e3ae1235cc5e5c
Prem-krish-308 <spremkumar128@gmail.com>
2026-05-02 19:50:50 +0530

    Change expected result of add test from 5 to 99

--- test/math.test.js
+++ test/math.test.js
@@ -1,5 +1,5 @@
 const { add, subtract, multiply } = require('../src/math');
 
-test('add: 2 + 3 = 5', () => { expect(add(2, 3)).toBe(5); });
+test('add: 2 + 3 = 5', () => { expect(add(2, 3)).toBe(99); });
 test('subtract: 10 - 4 = 6', () => { expect(subtract(10, 4)).toBe(6); });
 test('multiply: 3 * 4 = 12', () => { expect(multiply(3, 4)).toBe(12); });
